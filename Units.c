a7de983faff9cfb5e0cc2d7f87963bf8838baa3b
Jacinta26233 <jacintanduku4871@gmail.com>
2025-09-18 15:51:53 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,21 @@
+//simple C program (jacinta)
+/*
+Name:Sombe Jacinta Nduku
+Reg no:CT100/G/26233/25
+Description:Jacinta program
+*/
+
+#include <stdio.h>//
+
+//main function
+int main (){
+  printf("Analogue electronics/n");
+  printf("Computer systems and organization/n");
+  printf("Fundamentals of ICT/n");
+  printf("Introduction to computer application/n");
+  printf("Essentials of accounting/n");
+  printf("Communication skills/n");
+  printf("Mathematics for science/n");
+  printf("Introduction to programming and Algorithms");
+  return 0;
+}
