5720ee78a36f8acfd2c97621f51cc5b346952f2a
Jacinta26233 <jacintanduku4871@gmail.com>
2025-10-08 22:50:27 +0300

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,16 @@
+/*
+Name:Sombe Jacinta Nduku
+CT100/G/26233/25
+For  loop in C
+Description:program to print 100-50
+*/
+#include <stdio.h>
+
+int main(){
+  //for (start;stop;step);
+  int i;
+  for(i=100;i>=50;i--){
+    printf("%d\n",i);
+  }
+ return 0; 
+}
