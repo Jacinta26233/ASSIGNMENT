93527fc04f98c924a4329f592a9ac601353dc73d
Jacinta26233 <jacintanduku4871@gmail.com>
2025-10-23 18:01:07 +0300

    Add files via upload

---
+++ CalculateFare().c
@@ -0,0 +1,18 @@
+/*
+Name:Sombe Jacinta Nduku
+Reg.no:CT100/G/26233/25
+Description:A program to calculate fare
+*/
+#include <stdio.h>
+float calculateFare(float distance) {
+    return distance * 50;
+}
+
+int main() {
+    float distance;
+    printf("Enter distance traveled (in km): ");
+    scanf("%f", &distance);
+
+    printf("Total fare = KSh. %.2f\n", calculateFare(distance));
+    return 0;
+}
---
+++ convertToCelcius().c
@@ -0,0 +1,19 @@
+/*
+Name:Sombe Jacinta Nduku
+Reg.no:CT100/G/26233/25
+Description:A program to record temperature in celcius
+*/
+#include <stdio.h>
+
+float convertToCelsius(float fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+int main() {
+    float f;
+    printf("Enter temperature in Fahrenheit: ");
+    scanf("%f", &f);
+
+    printf("Temperature in Celsius = %.2f°C\n", convertToCelsius(f));
+    return 0;
+}
